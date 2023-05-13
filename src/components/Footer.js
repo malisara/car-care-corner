549c02ceef8f509d7c765219ed156d71457a3f1c
Sara Mali <mali.sara7@gmail.com>
2023-05-13 16:19:05 +0200

    footer changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,17 +8,27 @@ export default function Footer() {
     const bluishBg = useColorModeValue('offwhite', 'darkdarkblue');
 
     return (
-        <Flex as='footer'
+        <Flex //as='footer'
             bg={bluishBg}
-            h={50}
+            h='fit'
             alignItems='center'
             justifyContent='center'
-            px={1}>
-            <Text as='i' fontSize={12}>
+            py={4}
+            direction={{ base: 'column', lg: 'row' }}
+            fontSize={12}
+            lineHeight={5}
+        >
+            <Text as='b' >
                 Car Care Company
+            </Text>
+            <Text as='i'>
                 <PhoneIcon mx={4} />0123456789
+            </Text>
+            <Text as='i'>
                 <EmailIcon mx={4} />carcompany@mail.com
             </Text>
-        </Flex>
+
+
+        </Flex >
     );
 }
