0ee9f1d16e15f7cc995424e2cae95b8ab83d0588
Sara Mali <mali.sara7@gmail.com>
2023-07-06 20:49:32 +0200

    updated hero image

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,65 +1,37 @@
-import React from 'react';
+import React from "react";
 import { StaticImage } from "gatsby-plugin-image";
 
-import {
-    Heading,
-    Flex,
-    Text,
-    useColorModeValue,
-} from '@chakra-ui/react';
-
+import { Flex, Text, Box } from "@chakra-ui/react";
 
 export default function HeroBanner() {
-    const textColor = useColorModeValue('darkblue', 'offwhite');
-    const bgColor = useColorModeValue('offwhite', 'darkdarkblue');
-    return (
-        <Flex
-            height='fit'
-            bg={bgColor}
-            direction='column'
-            px={10}
-            py={15}
+  return (
+    <Flex height={{ base: 600, xl: 800 }} width="full">
+      <Flex height="full" width="100vw">
+        <StaticImage
+          src="../images/hero-image.png"
+          alt="Sport car"
+          style={{ width: "100%", height: "100%" }}
+          position="relative"
+        />
+        <Box
+          position="absolute"
+          top={"10rem"}
+          right={{ base: 0, xl: "10%" }}
+          width="fit"
+          height="auto"
+          p={10}
+          bg="rgba(0, 0, 0, 0.5)"
+          zIndex={1}
+          textAlign={{ base: "center", xl: "start" }}
         >
-
-            <Flex direction='column'
-                w={{ base: 'full', '2xl': '50%' }}
-                textAlign={{ base: 'center', xl: 'justify' }}
-                ms={{ xl: '60' }}
-                pt={{ xl: '50' }}>
-                <Heading sd='h1'
-                    size='3xl'
-                    color={textColor}>
-                    Car Care Corner
-                </Heading>
-
-                <Text fontSize={{ base: 'lg', md: '2xl' }} as='b' pt={5}
-                    color={textColor}>
-                    Drive in style,
-                    maintain & repair like pro
-                </Text>
-
-                <Text
-                    py={5}
-                    w={{ base: 'full', xl: '60%' }}
-                    as='i'
-                    lineHeight={7}
-                >
-                    Welcome to our one-stop shop for vehicle owners and enthusiasts!
-
-                    Our comprehensive inventory includes everything required to maintain and
-                    repair your beloved ride. Whether you're looking for a specific
-                    part or want to give your car a fresh look, we've got you
-                    covered
-                </Text>
-            </Flex>
-
-            <Flex mt={{ base: '10', xl: '0' }} me={{ base: '0', xl: '60' }}
-                justifyContent='end' >
-                <StaticImage src="../images/hero-image-2.png"
-                    alt="A gray sport car" />
-            </Flex>
-
-        </Flex >
-
-    );
+          <Text color="yellow" fontWeight="bold" fontSize="5xl" mb={2}>
+            Car Care Corner
+          </Text>
+          <Text color="white" fontSize="xl">
+            Drive in style, maintain & repair like pro
+          </Text>
+        </Box>
+      </Flex>
+    </Flex>
+  );
 }
